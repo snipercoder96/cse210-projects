0685a804b517fe6cf2639d1a2c2b85cf473f37c0
snipercoder96 <kmbembakiyindou@byupathway.edu>
2025-10-27 19:15:38 +0200

    Done exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade: ");
+        string valueFromUser = Console.ReadLine();
+        int integerValue = int.Parse(valueFromUser);
+
+        string letter = "";
+        string signedValue = "";
+
+        int lastDigit = integerValue % 10;
+
+        // If remained digits = 7 or 3
+        if (lastDigit >= 7)
+        {
+            signedValue = "+";
+        }
+        else if (lastDigit <= 3)
+        {
+            signedValue = "-";
+        }
+
+        if (integerValue >= 90)
+        {
+            letter = "A";
+        }
+        else if (integerValue >= 80)
+        {
+            letter = "B";
+        }
+        else if (integerValue >= 70)
+        {
+            letter = "C";
+        }
+        else if (integerValue >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Signed values
+        if (letter == "A" && signedValue == "+")
+        {
+            signedValue = "";
+        }
+
+        if (letter == "F" && (signedValue == "+" || signedValue == "-"))
+        {
+            signedValue = "";
+        }
+
+        Console.WriteLine($"You have a grade {letter}{signedValue}.");
+
+        if (integerValue >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed the course with a {letter}");
+        }
+        else
+        {
+            Console.WriteLine($" The grade is {letter}. Don't give up — you'll do better next time!");
+        }
     }
 }
