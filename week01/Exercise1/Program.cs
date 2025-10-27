a95581c196a3c2412f510d063a96b819f1253401
snipercoder96 <kmbembakiyindou@byupathway.edu>
2025-10-27 17:28:40 +0200

    Practiced Week 1 excercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,7 +3,15 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    {   
+
+        // Create Input for the user
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Example of how Sting Interpolation is used
+        Console.Write($"Your name is {first_name}, {first_name} {last_name}.");
     }
 }
