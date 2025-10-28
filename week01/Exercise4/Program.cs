e1639146b414b56256285ec6b3870497997cbf3c
snipercoder96 <kmbembakiyindou@byupathway.edu>
2025-10-28 18:12:24 +0200

    Exercise 4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Message
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Create an empty list
+        List<int> numbers = new List<int>();
+
+        while (true)
+        {
+
+            // Initialize the questions
+            Console.Write("Enter number: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == 0)
+            {
+                break;
+            }
+
+            numbers.Add(userInput);
+        }
+
+        int sum = 0;
+        int max = int.MinValue;
+        int min = int.MaxValue;
+
+
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+
+            if (num < min)
+            {
+                min = num;
+            }
+
+            sum += num;
+
+        }
+
+        double average = sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The maximum value is: {max}");
+        Console.WriteLine($"The minimum value is: {min}");
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
