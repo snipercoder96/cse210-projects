da6a6972234760db9d38208324929d8f64c570d3
snipercoder96 <kmbembakiyindou@byupathway.edu>
2025-11-12 14:06:52 +0200

    Week03 code along activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,46 @@
+/*
+Designing the Fractions class
+*/
+
+class Fraction
+{   
+
+    // Principle of Encapsulation
+    private int _top;
+    private int _bottom;
+
+    // Create 3 constructors (This is known as Constructor overloading)
+
+    // Constructor 1 to take no parameters
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+
+    //Constructor 2 takes only 1 parameter
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    // Constructor 3 takes 2 parameters
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Create Get and Setters
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / _bottom;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // No parameters
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());  
+        Console.WriteLine(f1.GetDecimalValue());     
+
+        // One parameter
+        Fraction f2 = new Fraction(6);
+        Console.WriteLine(f2.GetFractionString());   
+        Console.WriteLine(f2.GetDecimalValue());     
+
+        // Two parameters
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine(f3.GetFractionString());   
+        Console.WriteLine(f3.GetDecimalValue());     
+
+
+
     }
 }
